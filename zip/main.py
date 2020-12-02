41a8996c4e352ae1c690131a224d6f4f9252a02c
朱世伟 <zgamecn@gmail.com>
2020-12-02 18:12:12 +0800

    add

--- zip/main.py
+++ zip/main.py
@@ -4,11 +4,13 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import zlib
 import gzip
+import snappy
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+    print('-------------zlib---------------')
     message = 'hello'
     compressed = zlib.compress(message.encode())
     decompressed = zlib.decompress(compressed)
@@ -19,14 +21,22 @@ def print_hi(name):
     # with open('1.txt', 'w', encoding='utf-8') as f:  # python3
     #     f.write(compressed.decode(encoding="utf-8"))
 
-    print('----------------------------')
+    print('-------------gzip---------------')
     f_in = open("1.txt", "rb")  # 打开文件
     f_out = gzip.open("data.txt.gz", "wb")  # 创建压缩文件对象
     f_out.writelines(f_in)
     f_out.close()
     f_in.close()
 
+    print('-------------snappy---------------')
+    compressed = snappy.compress(message)
+    print_hex(compressed)
+    decompressed = snappy.uncompress(compressed)
+    print('uncompressed:', repr(decompressed))
 
+
+
+# 打印16进制
 def print_hex(bytes):
     l = [hex(int(i)) for i in bytes]
     print(" ".join(l))
